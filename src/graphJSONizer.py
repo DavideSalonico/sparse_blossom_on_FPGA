4c22ce2252c3147e6c10c844737527e109a899de
DavideSalonico <davide.salonico@gmail.com>
2024-06-23 12:40:09 +0200

    updated JSONizer

--- src/graphJSONizer.py
+++ src/graphJSONizer.py
@@ -15,17 +15,24 @@ matching = pymatching.Matching.from_detector_error_model(model)
 edges = matching.edges()
 n_dets = matching.num_detectors
 n_nodes = matching.num_nodes
+n_fault_ids = matching.num_fault_ids
+n_edges = matching.num_edges
 
+"""
 # Function to exclude specific attributes from the dictionary
 def serialize_item(item, exclude_key='fault_ids'):
     if isinstance(item, tuple) and len(item) == 3 and isinstance(item[2], dict):
         item_dict = {k: v for k, v in item[2].items() if k != exclude_key}
         return (item[0], item[1], item_dict)
     return item
+"""
+
+edges_fault = [edge in edges if edge[2]["fault_ids"] != set()]
+print(edges_fault)
 
 # Initialize an empty dictionary for nodes
 nodes = {}
-node_index = 0
+#node_index = 0
 
 # Iterate over each edge to populate the nodes dictionary
 for edge in edges:
@@ -34,9 +41,9 @@ for edge in edges:
     for node, neighbor in [(node1, node2), (node2, node1)]:
         if node not in nodes:
             # Assign the current index to the node and increment the index counter
-            nodes[node] = {"index": node_index, "region_idx": None, "neighbors": [], "neigh_weights": [], "neigh_obs": []}
-            node_index += 1
-        if len(nodes[node]["neighbors"]) < 4:  # Ensure a maximum of 4 neighbors
+            nodes[node] = {"index": node, "neighbors": [], "neigh_weights": [], "neigh_obs": []}
+            #node_index += 1
+        #if len(nodes[node]["neighbors"]) < 4:  # Ensure a maximum of 4 neighbors
             nodes[node]["neighbors"].append(neighbor)
             nodes[node]["neigh_weights"].append(attributes.get("weight", 1))  # Default weight to 1 if not present
             nodes[node]["neigh_obs"].append(attributes.get("fault_ids", []))  # Default fault_ids to empty list if not present
