b37d5c8f2b89ba9a13d89437309164f60f12fd59
DadoPres <davide.salonico@gmail.com>
2024-06-23 14:39:04 +0200

    update JSONizer

--- src/graphJSONizer.py
+++ src/graphJSONizer.py
@@ -16,45 +16,42 @@ edges = matching.edges()
 n_dets = matching.num_detectors
 n_nodes = matching.num_nodes
 n_fault_ids = matching.num_fault_ids
+print("Number of faults: ", n_fault_ids)
 n_edges = matching.num_edges
 
-"""
 # Function to exclude specific attributes from the dictionary
-def serialize_item(item, exclude_key='fault_ids'):
+def serialize_item(item):
     if isinstance(item, tuple) and len(item) == 3 and isinstance(item[2], dict):
-        item_dict = {k: v for k, v in item[2].items() if k != exclude_key}
+        item_dict = {k: (list(v) if isinstance(v, set) else v) for k, v in item[2].items()}
         return (item[0], item[1], item_dict)
     return item
-"""
 
-edges_fault = [edge in edges if edge[2]["fault_ids"] != set()]
-print(edges_fault)
+serialized_edges = [serialize_item(edge) for edge in edges]
+with open('edges.json', 'w') as json_file:
+    json.dump({"edges": list(serialized_edges)}, json_file, indent=4)
 
-# Initialize an empty dictionary for nodes
 nodes = {}
-#node_index = 0
 
-# Iterate over each edge to populate the nodes dictionary
-for edge in edges:
-    serialized_edge = serialize_item(edge)
-    node1, node2, attributes = serialized_edge
-    for node, neighbor in [(node1, node2), (node2, node1)]:
-        if node not in nodes:
-            # Assign the current index to the node and increment the index counter
-            nodes[node] = {"index": node, "neighbors": [], "neigh_weights": [], "neigh_obs": []}
-            #node_index += 1
-        #if len(nodes[node]["neighbors"]) < 4:  # Ensure a maximum of 4 neighbors
-            nodes[node]["neighbors"].append(neighbor)
-            nodes[node]["neigh_weights"].append(attributes.get("weight", 1))  # Default weight to 1 if not present
-            nodes[node]["neigh_obs"].append(attributes.get("fault_ids", []))  # Default fault_ids to empty list if not present
+for edge in serialized_edges:
+    node1, node2, attrs = edge
+    if node1 not in nodes:
+        nodes[node1] = {"index": node1, "neighbors": [], "neigh_weights": [], "neigh_obs": []}
+    if node2 not in nodes:
+        nodes[node2] = {"index": node2, "neighbors": [], "neigh_weights": [], "neigh_obs": []}
 
-# Assuming nodes is your dictionary that you want to serialize
-# Convert all sets to lists in the nodes dictionary
-for node, data in nodes.items():
-    for key in ['neighbors', 'neigh_weights', 'neigh_obs']:
-        if isinstance(data[key], set):
-            data[key] = list(data[key])
+for edge in serialized_edges:
+    node1, node2, attrs = edge
+    if node1 != None and node2 != None:
+        nodes[node1]["neighbors"].append(node2)
+        nodes[node1]["neigh_weights"].append(attrs["weight"])
+        nodes[node1]["neigh_obs"].append(attrs["fault_ids"])
+        
+        nodes[node2]["neighbors"].append(node1)
+        nodes[node2]["neigh_weights"].append(attrs["weight"])
+        nodes[node2]["neigh_obs"].append(attrs["fault_ids"])
+    
+#print(nodes)
 
 # Now, serialize the nodes dictionary to JSON
 with open('nodes.json', 'w') as json_file:
-    json.dump({"n_dets": n_dets, "n_nodes": n_nodes, "nodes": list(nodes.values())}, json_file, indent=4)
+    json.dump({"n_dets": n_dets, "n_nodes": n_nodes, "nodes": list(nodes.values())}, json_file, indent=4)    
